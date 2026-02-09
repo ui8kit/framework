f8cbebf501d87c41f64451989b52408d70c7329c
alexy-os <agorbunoff.ru@gmail.com>
2026-02-09 18:33:48 +0300

    Sputnik moment. The first working prototype of the quadricycle rolled out of the garage: a framework that uses DSL language to convert React into popular, production-ready templates. Is the door too narrow, like in Ford's coal garage? An axe to the bricks - the first application is deployed! Forward, to great goals! / NOTE: The sputnik moment occurred yesterday (8 Feb 2026) in another thread, which will be deleted. After minor edits, we are releasing this still raw version, but one that is already running smoothly.

--- apps/engine/src/layouts/index.ts
+++ apps/engine/src/layouts/index.ts
@@ -2,8 +2,7 @@
 export { MainLayout } from './MainLayout';
 export type { MainLayoutProps, LayoutMode } from './MainLayout';
 
-export { DashLayout, Dashboard } from './DashLayout';
-export type { DashboardProps } from './DashLayout';
+export { DashLayout } from './DashLayout';
 
 // Re-export partial types for convenience
 export type { NavItem, FooterSection, FooterLink } from '../partials';
